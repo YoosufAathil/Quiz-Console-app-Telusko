91fb1bac7cf05bd8315a3d860889d2b275629051
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-08-01 12:09:50 +0530

    chore: Add printScore method to display user's quiz score

--- QuestionService.java
+++ QuestionService.java
@@ -30,4 +30,21 @@ public class QuestionService {
             System.out.println(s);
         }
     }
+
+    public void printScore() {
+        int score = 0;
+        for (int i = 0; i < questions.length; i++) {
+
+            Question que = questions[i];
+            String actualAnswer = que.getAnswer();
+
+            String userAnswer = selection[i];
+
+            if (actualAnswer.equals(userAnswer)) {
+                score++;
+            }
+
+        }
+        System.out.println("Your score is : " + score);
+    }
 }
