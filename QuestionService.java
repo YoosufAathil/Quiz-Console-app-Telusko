7edcb73bcf5819f36723352c06dd2ed4d3721e64
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-08-01 12:02:01 +0530

    chore: Refactor code to improve user experience in quiz game

--- QuestionService.java
+++ QuestionService.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class QuestionService {
     Question[] questions = new Question[5];
+    String[] selection = new String[5];
 
     public QuestionService() {
         questions[0] = new Question(1, "What is 1 + 1?", "1", "2", "3", "4", "2");
@@ -9,10 +12,22 @@ public class QuestionService {
         questions[4] = new Question(5, "What is 5 + 5?", "8", "9", "10", "11", "10");
     }
 
-    public void displayQuestions() {
+    public void playQuiz() {
+        int i = 0;
         for (Question q : questions) {
-            System.out.println(q);
+            System.out.println("Question No : " + q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println("1. " + q.getOpt1());
+            System.out.println("2. " + q.getOpt2());
+            System.out.println("3. " + q.getOpt3());
+            System.out.println("4. " + q.getOpt4());
+            Scanner sc = new Scanner(System.in);
+            selection[i] = sc.nextLine();
+            i++;
 
+        }
+        for (String s : selection) {
+            System.out.println(s);
         }
     }
 }
